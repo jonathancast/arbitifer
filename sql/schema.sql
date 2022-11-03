2da6a37a3b1c93bf294e833872912f70dfc9c024
Jonathan Cast <jonathanccast@fastmail.fm>
2022-11-02 19:23:57 -0500

    [1] Update auditing on tickets table to handle users as well as dates

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,15 @@
 create sequence seq_ticket_no;
 
+create or replace function arbitifer_user() returns text as $$
+begin
+    return coalesce(current_setting('arbitifer.username', true), current_user);
+end;
+$$ language 'plpgsql';
+
 create table tickets (
     id integer primary key default nextval('seq_ticket_no'),
     story text not null,
-    created_by text not null,
+    created_by text not null default arbitifer_user(),
     created_at timestamp with time zone not null default now(),
     updated_by text,
     updated_at timestamp with time zone
@@ -12,6 +18,7 @@ create table tickets (
 create or replace function update_modified_column()
 returns trigger AS $$
 begin
+    NEW.updated_by = arbitifer_user();
     NEW.updated_at = now();
     return NEW;
 end;
